b968383d3ceb7d74aeaf0d7c7f41dae24d35f28b
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-08-15 11:35:21 +0600

    tailwind and daisy ui add

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+    </div>
+  );
 };
 
 export default HomePage;

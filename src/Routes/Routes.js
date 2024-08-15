fede3553672005f118c45cbfc02198c03b1010ce
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-08-15 11:27:56 +0600

    route update

---
+++ src/Routes/Routes.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Main from "../Layout/Main";
+import HomePage from "../Pages/Home/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Main />}>
+        <Route index element={<HomePage />} />
+        {/* <Route
+          path="jobs/create-job"
+          element={
+            <PrivateRoute>
+              <JobForm />
+            </PrivateRoute>
+          }
+        /> */}
+        {/* <Route
+          path="about"
+          element={
+            <PrivateRoute>
+              <AboutPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="contact"
+          element={
+            <PrivateRoute>
+              <ContactPage />
+            </PrivateRoute>
+          }
+        /> */}
+        {/* <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route> */}
+      </Route>
+      {/* <Route path="/jobs" element={<JobLayout />}>
+        <Route
+          index
+          element={
+            <PrivateRoute>
+              <AllJob />
+            </PrivateRoute>
+          }
+        />
+      </Route> */}
+    </>
+  )
+);
+
+export default router;
